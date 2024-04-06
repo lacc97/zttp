bd5009ac9c2e2be41799745ba50e5c64955d0097
Luis Cáceres <lacc97@protonmail.ch>
2024-04-06 19:28:16 +0100

    Make the HTTP/1.1 state machine generic
    The StateMachine type is parametrised by a Context type and a pair
    of handling functions.
    
    This commit also moves the request parsing tests into the more general
    state machine tests.

--- src/http1.zig
+++ src/http1.zig
@@ -6,71 +6,142 @@ const constants = @import("constants.zig");
 
 const Request = @import("Request.zig");
 
-pub const StateMachine = union(enum) {
-    headers,
-    body: struct {
-        cur: u64 = 0,
-        len: u64,
-    },
-    body_chunked: struct { chunk: ?struct {} },
+pub fn StateMachine(
+    comptime Context: type,
+    comptime on_request: anytype,
+    comptime on_data: anytype,
+) type {
+    return struct {
+        inner: union(enum) {
+            headers,
+            body: Chunk,
+            body_chunked: ?Chunk,
+        } = .headers,
+        context: Context = undefined,
 
-    pub fn process(sm: *StateMachine, data: []u8) !usize {
-        return switch (sm.*) {
-            .headers => blk_headers: {
-                var req: Request = undefined;
-                const consumed = try parseRequestLineAndHeaders(&req, data);
+        pub fn process(sm: *@This(), data: []u8) !usize {
+            return switch (sm.inner) {
+                .headers => blk_headers: {
+                    var req: Request = undefined;
+                    const consumed = try parseRequestLineAndHeaders(&req, data);
 
-                const transfer_encoding = getTransferEncoding(&req.headers) catch {
-                    return error.InvalidFraming;
-                };
-                const content_length = getContentLength(&req.headers) catch {
-                    return error.InvalidFraming;
-                };
-
-                if (transfer_encoding) |te| {
-                    if (content_length != null) {
+                    const transfer_encoding = getTransferEncoding(&req.headers) catch {
                         return error.InvalidFraming;
+                    };
+                    const content_length = getContentLength(&req.headers) catch {
+                        return error.InvalidFraming;
+                    };
+
+                    if (transfer_encoding) |te| {
+                        if (content_length != null) {
+                            return error.InvalidFraming;
+                        }
+                        if (te != .chunked) {
+                            return error.UnsupportedCoding;
+                        }
+                        sm.inner = .{ .body_chunked = null };
+                    } else {
+                        const length = if (content_length) |ce| ce else 0;
+
+                        // If the length is zero, the message has no body and would be
+                        // immediately followed by another request (if any).
+                        if (length != 0) {
+                            sm.inner = .{ .body = .{ .cur = 0, .len = length } };
+                        }
                     }
-                    if (te != .chunked) {
-                        return error.UnsupportedCoding;
-                    }
-                    sm.* = .{ .body_chunked = .{ .chunk = null } };
-                } else {
-                    const length = if (content_length) |ce| ce else 0;
-
-                    // If the length is zero, the message has no body and would be
-                    // immediately followed by another request (if any).
-                    if (length != 0) {
-                        sm.* = .{ .body = .{ .cur = 0, .len = length } };
-                    }
-                }
-                break :blk_headers consumed;
-            },
-            .body => |*body| blk_body: {
-                const consumed = @min(@as(u64, data.len), body.len - body.cur);
-                body.cur += consumed;
-                if (body.cur == body.len) sm.* = .headers;
-                break :blk_body consumed;
-            },
-            .body_chunked => |*body| {
-                _ = body;
-                @panic("TODO: RFC9112 section 7.1");
-            },
-        };
-    }
-
-    test process {
-        for (test_cases_correct) |case| {
-            const buf = try testing.allocator.dupe(u8, case.bytes);
-            defer testing.allocator.free(buf);
-
-            var sm: StateMachine = .headers;
-
-            try testing.expectError(error.NeedMore, sm.process(buf[0 .. buf.len / 2]));
-            try testing.expectEqual(@as(usize, buf.len), sm.process(buf));
+                    try sm.onRequest(req);
+                    break :blk_headers consumed;
+                },
+                .body => |*body| blk_body: {
+                    const consumed = @min(@as(u64, data.len), body.len - body.cur);
+                    body.cur += consumed;
+                    if (body.cur == body.len) sm.inner = .headers;
+                    try sm.onData(data[0..consumed]);
+                    break :blk_body consumed;
+                },
+                .body_chunked => |*body| {
+                    _ = body;
+                    @panic("TODO: RFC9112 section 7.1");
+                },
+            };
         }
-    }
+
+        fn onRequest(sm: *@This(), req: Request) !void {
+            // TODO: check function, handle errors
+            return @call(.always_inline, on_request, .{ &sm.context, req });
+        }
+        fn onData(sm: *@This(), data: error{Closed}![]u8) !void {
+            // TODO: check function, handle errors
+            return @call(.always_inline, on_data, .{ &sm.context, data });
+        }
+    };
+}
+
+const Chunk = struct {
+    cur: u64 = 0,
+    len: u64,
 };
+
+test StateMachine {
+    for (test_cases_correct) |case| {
+        const Header = @typeInfo(@TypeOf(case.headers)).Pointer.child;
+        const Context = struct {
+            case: @TypeOf(case),
+            request_received: bool = false,
+            data_received: u64 = 0,
+
+            fn onRequest(ctx: *@This(), req: Request) !void {
+                try testing.expectEqualStrings(ctx.case.method, req.method);
+                try testing.expectEqualStrings(ctx.case.path, req.path);
+
+                const headers = try testing.allocator.alloc(Header, req.headers.len());
+                defer testing.allocator.free(headers);
+
+                {
+                    var i: usize = 0;
+                    var it = req.headers.iterator();
+                    while (it.next()) |hdr| : (i += 1) headers[i] = .{ .name = hdr.name, .value = hdr.value };
+                    try testing.expectEqual(headers.len, i);
+                }
+
+                std.sort.pdq(Header, headers, Header.SortContext{}, Header.SortContext.lessThan);
+
+                for (0..@max(headers.len, ctx.case.headers.len)) |i| {
+                    const name_expected = if (ctx.case.headers.len > i) ctx.case.headers[i].name else "";
+                    const value_expected = if (ctx.case.headers.len > i) ctx.case.headers[i].value else "";
+                    const name_got = if (headers.len > i) headers[i].name else "";
+                    const value_got = if (headers.len > i) headers[i].value else "";
+
+                    try testing.expectEqualStrings(name_expected, name_got);
+                    try testing.expectEqualStrings(value_expected, value_got);
+                }
+
+                ctx.request_received = true;
+            }
+
+            fn onData(ctx: *@This(), data: error{Closed}![]u8) !void {
+                _ = ctx;
+                _ = data catch @panic("we're closed");
+                @panic("TODO: ");
+            }
+        };
+
+        const SM = StateMachine(
+            Context,
+            Context.onRequest,
+            Context.onData,
+        );
+
+        const buf = try testing.allocator.dupe(u8, case.bytes);
+        defer testing.allocator.free(buf);
+
+        var sm: SM = .{ .context = .{ .case = case } };
+
+        try testing.expectError(error.NeedMore, sm.process(buf[0 .. buf.len / 2]));
+        try testing.expectEqual(@as(usize, buf.len), sm.process(buf));
+        try testing.expect(sm.context.request_received);
+    }
+}
 
 fn getTransferEncoding(hm: *const Request.HeaderMap) error{Invalid}!?TransferEncoding {
     // TODO: get transfer-encoding stack and verify that the last coding is chunked (RFC9112 section 6.1)
@@ -105,45 +176,6 @@ fn parseRequestLineAndHeaders(req: *Request, data: []u8) !usize {
     try p.parseRequestLine();
     while (try p.parseRequestHeader()) {}
     return p.curr;
-}
-
-test parseRequestLineAndHeaders {
-    for (test_cases_correct) |case| {
-        const CaseHeader = @typeInfo(@TypeOf(case.headers)).Pointer.child;
-
-        const buf = try testing.allocator.dupe(u8, case.bytes);
-        defer testing.allocator.free(buf);
-
-        var req: Request = undefined;
-
-        const consumed = try parseRequestLineAndHeaders(&req, buf);
-        try testing.expectEqual(@as(usize, case.bytes.len), consumed);
-
-        try testing.expectEqualStrings(case.method, req.method);
-        try testing.expectEqualStrings(case.path, req.path);
-
-        const headers = try testing.allocator.alloc(CaseHeader, req.headers.len());
-        defer testing.allocator.free(headers);
-
-        {
-            var i: usize = 0;
-            var it = req.headers.iterator();
-            while (it.next()) |hdr| : (i += 1) headers[i] = .{ .name = hdr.name, .value = hdr.value };
-            try testing.expectEqual(headers.len, i);
-        }
-
-        std.sort.pdq(CaseHeader, headers, CaseHeader.SortContext{}, CaseHeader.SortContext.lessThan);
-
-        for (0..@max(headers.len, case.headers.len)) |i| {
-            const name_expected = if (case.headers.len > i) case.headers[i].name else "";
-            const value_expected = if (case.headers.len > i) case.headers[i].value else "";
-            const name_got = if (headers.len > i) headers[i].name else "";
-            const value_got = if (headers.len > i) headers[i].value else "";
-
-            try testing.expectEqualStrings(name_expected, name_got);
-            try testing.expectEqualStrings(value_expected, value_got);
-        }
-    }
 }
 
 const RequestParser = struct {

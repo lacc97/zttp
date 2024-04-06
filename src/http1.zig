80504a5fa1aab4de5f1b15c5d54cbe93212b69d3
Luis Cáceres <lacc97@protonmail.ch>
2024-04-06 02:32:01 +0100

    Simplify HTTP/1.1 state machine
    Buffer management should be a responsibility of the caller, whereas
    the state machine simply tells how much it processed. If it couldn't
    advance (i.e. if there was an error or if there simply wasn't enough
    data to do anything) the caller receives an error. The caller is
    expected to provide enough data in a contiguous buffer.
    
    In future, this type should be parametrised on functions that handle
    a complete Request and the request body data.

--- src/http1.zig
+++ src/http1.zig
@@ -6,52 +6,32 @@ const constants = @import("constants.zig");
 
 const Request = @import("Request.zig");
 
-pub const Context = struct {
-    request: Request = undefined,
-    state: enum {
-        headers,
-        body,
-        body_chunked,
-    } = .empty,
-    buffer: Buffer(constants.http1_buffer_max, u16) = .{},
+pub const StateMachine = union(enum) {
+    headers,
+    body: struct {
+        cur: u64 = 0,
+        len: u64,
+    },
+    body_chunked,
 
-    pub const ParseError = error{};
-
-    pub fn feed(ctx: *Context, data: []u8) !void {
-        switch (ctx.state) {
-            .headers => {
-                const data_consumed = if (ctx.buffer.readableSlice().len > 0) blk_consume: {
-                    // Already have some buffered up, so we append as much as we can to the buffer and try to parse.
-                    const data_consumed = ctx.buffer.putSome(data);
-                    const consumed = ctx.request.parse(ctx.buffer.readableSlice()) catch |err| return switch (err) {
-                        error.NeedMore => if (ctx.buffer.writeableSlice().len == 0) error.TooLarge else error.NeedMore,
-                        else => |e| e,
-                    };
-                    ctx.buffer.consume(consumed);
-                    break :blk_consume data_consumed;
-                } else ctx.request.parse(data) catch |err| return switch (err) {
-                    error.NeedMore => if (data.len <= ctx.buffer.capacity()) blk: {
-                        ctx.buffer.appendSliceAssumeCapacity(data);
-                        break :blk error.NeedMore;
-                    } else error.TooLarge,
-                    else => |e| e,
-                };
-
-                std.debug.print("method: {s}, path: {s}, version: {s}, headers: {}\n", .{ ctx.request.method, ctx.request.path, @tagName(ctx.request.version), ctx.request.headers.slice() });
-
-                _ = data_consumed;
+    pub fn process(sm: *StateMachine, data: []u8) !usize {
+        return switch (sm.*) {
+            .headers => blk_headers: {
+                var req: Request = undefined;
+                const consumed = try parseRequestLineAndHeaders(&req, data);
+                req.getHeadersByName();
+                break :blk_headers consumed;
             },
-        }
+        };
     }
 };
 
-fn parse(req: *Request, data: []u8) !usize {
+fn parseRequestLineAndHeaders(req: *Request, data: []u8) !usize {
     assert(data.len > 0);
 
     req.headers = .{};
-    errdefer req.* = undefined;
 
-    var p: Parser = .{
+    var p: RequestParser = .{
         .req = req,
         .data = data,
         .curr = 0,
@@ -62,7 +42,7 @@ fn parse(req: *Request, data: []u8) !usize {
     return p.curr;
 }
 
-test parse {
+test parseRequestLineAndHeaders {
     const cases = blk: {
         const tc = @import("test-cases").http1;
         break :blk [_]tc.Request{ tc.request_simple, tc.request_ziglang_docs };
@@ -76,7 +56,7 @@ test parse {
 
         var req: Request = undefined;
 
-        const consumed = try req.parse(buf);
+        const consumed = try parseRequestLineAndHeaders(&req, buf);
         try testing.expectEqual(@as(usize, case.bytes.len), consumed);
 
         try testing.expectEqualStrings(case.method, req.method);
@@ -100,12 +80,12 @@ test parse {
     }
 }
 
-const Parser = struct {
+const RequestParser = struct {
     req: *Request,
     data: []u8,
     curr: usize,
 
-    fn parseRequestLine(p: *Parser) !void {
+    fn parseRequestLine(p: *RequestParser) !void {
         { // -- method
             const sp_off = for (p.data[p.curr..], 0..) |ch, i| {
                 if (!isMethodChar(ch)) {
@@ -135,7 +115,7 @@ const Parser = struct {
         }
     }
 
-    fn parseRequestHeader(p: *Parser) !bool {
+    fn parseRequestHeader(p: *RequestParser) !bool {
         if (p.remainingLen() < 2) return error.NeedMore;
         if (p.data[p.curr] == '\r' and p.data[p.curr + 1] == '\n') {
             _ = p.advance(2);
@@ -172,13 +152,13 @@ const Parser = struct {
         return true;
     }
 
-    inline fn advance(p: *Parser, n: usize) []u8 {
+    inline fn advance(p: *RequestParser, n: usize) []u8 {
         assert(p.remainingLen() >= n);
         defer p.curr += n;
         return p.data[p.curr..][0..n];
     }
 
-    inline fn remainingLen(p: *Parser) usize {
+    inline fn remainingLen(p: *RequestParser) usize {
         return p.data.len - p.curr;
     }
 
@@ -196,117 +176,3 @@ const Parser = struct {
         return std.ascii.isAlphabetic(ch) or ch == '-';
     }
 };
-
-pub const BufferSize = union(enum) {
-    static: usize,
-    dynamic,
-};
-
-pub fn Buffer(comptime size: BufferSize, comptime IndexType: type) type {
-    return struct {
-        const Self = @This();
-
-        r_cursor: Index = 0,
-        w_cursor: Index = 0,
-        buffer_len: switch (size) {
-            .static => void,
-            .dynamic => Index,
-        } = switch (size) {
-            .static => {},
-            .dynamic => 0,
-        },
-        buffer: switch (size) {
-            .static => |s| [s]u8,
-            .dynamic => [*]u8,
-        } = undefined,
-
-        pub fn initDynamic(buffer: []u8) Self {
-            return switch (size) {
-                .static => @compileError("static buffer cannot be initialised as dynamic"),
-                .dynamic => blk: {
-                    assert(buffer.len <= std.math.maxInt(Index));
-                    break :blk .{ .buffer_len = @intCast(buffer.len), .buffer = buffer.ptr };
-                },
-            };
-        }
-
-        pub const Index = blk_index: {
-            switch (@typeInfo(IndexType)) {
-                .Int => |info| {
-                    // Check that indices are unsigned.
-                    if (info.signedness != .unsigned) @compileError("index type must be unsigned (got " ++ @typeName(IndexType) ++ ")");
-                    // Check that the len/capacity can always be coerced to a usize.
-                    if (info.bits > @bitSizeOf(usize)) @compileError("index type is wider than usize (got" ++ @typeName(IndexType) ++ ")");
-                    switch (size) {
-                        .static => |s| if (std.math.maxInt(IndexType) < s) @compileError(std.fmt.comptimePrint(
-                            "index type is too narrow to address static storage ({s} can only address up to {} bytes, but need to address at least {} bytes)",
-                            .{ @typeName(IndexType), std.math.maxInt(IndexType), s },
-                        )),
-                        else => {},
-                    }
-                    break :blk_index IndexType;
-                },
-                else => @compileError("index type must be an integer (got " ++ @typeName(IndexType) ++ ")"),
-            }
-        };
-
-        pub inline fn capacity(b: *const Self) usize {
-            return switch (size) {
-                .static => |s| s,
-                .dynamic => b.buffer_len,
-            };
-        }
-
-        /// Fully resets the buffer.
-        pub fn reset(b: *Self) void {
-            b.r_cursor = 0;
-            b.w_cursor = 0;
-        }
-
-        /// Resets the buffer but keeps the part that hasn't been read yet and moves it to the front.
-        pub fn resetAndKeep(b: *Self) void {
-            if (b.r_cursor != 0) {
-                assert(b.r_cursor <= b.w_cursor);
-                const readable = b.readableSlice();
-                std.mem.copyForwards(u8, b.storage(), readable);
-                b.r_cursor = 0;
-                b.w_cursor = readable.len;
-            }
-        }
-
-        /// Copies as much as it can from `bytes` into the internal buffer, bumping up the write cursor as required. Returns how much was copied.
-        pub fn putSome(b: *Self, bytes: []const u8) usize {
-            const write_buffer = b.writeableSlice();
-            const write_len = @min(write_buffer.len, bytes.len);
-            @memcpy(write_buffer[0..write_len], bytes[0..write_len]);
-            b.w_cursor += @as(Index, @intCast(write_len));
-            assert(b.w_cursor <= b.capacity());
-            return write_len;
-        }
-
-        /// Consumes part of the read buffer, bumping up the read cursor as required. The `count` parameter must not exceed the size of the readable slice.
-        pub fn consume(b: *Self, count: usize) void {
-            const read_len = @as(Index, @intCast(count));
-            b.r_cursor += read_len;
-            assert(b.r_cursor <= b.w_cursor);
-        }
-
-        /// The part of the buffer that is available for writes.
-        pub inline fn writeableSlice(b: *Self) []u8 {
-            return b.storage()[b.w_cursor..b.capacity()];
-        }
-
-        /// The part of the buffer that is available for reads.
-        pub inline fn readableSlice(b: *Self) []u8 {
-            return b.storage()[0..b.r_cursor];
-        }
-
-        inline fn storage(b: *Self) []u8 {
-            return b.buffer[0..b.capacity()];
-        }
-    };
-}
-
-test {
-    _ = Request;
-}
